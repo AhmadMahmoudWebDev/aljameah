edfcfe6c6bbcb6769b2b0aab5b3c9e609caa4d61
Ahmad Mahmoud <ahmadmahmoud.webdev@gmail.com>
2020-04-09 01:51:52 +0300

    add landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,87 +1,57 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
+  <v-container fluid>
+    <v-row>
+      <v-col
+        v-for="(item, i) in items"
+        :key="i"
+        cols="12"
+        sm="6"
+        md="6"
+        lg="6"
+        xl="3"
+      >
+        <v-card
+          :color="item.color"
+          :to="item.link"
+          dark
+          shaped
+          hover
+          ripple
+          nuxt
+        >
+          <div class="d-flex flex-no-wrap justify-space-between">
+            <div>
+              <v-card-title
+                class="headline"
+                v-text="item.title"
+              />
+
+              <v-card-subtitle v-text="item.description" />
+            </div>
+
+            <v-avatar
+              class="ma-3"
+              size="75"
+              tile
             >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
+              <v-icon :color="item.iconColor" size="75">
+                {{ item.icon }}
+              </v-icon>
+            </v-avatar>
           </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 
 export default {
-  // data
+  computed: {
+    items () {
+      return this.$store.state.mainCategories.items
+    }
+  }
 }
 </script>
